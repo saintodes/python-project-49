e123a1058e0a0b64b266f476fb072822f1b053a3
saintodes <odes36@gmail.com>
2023-03-03 19:10:47 +0700

    cli.py PEP8 check

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,10 @@
 import prompt
 
+
 def welcome_user():
     print('Whelcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
-welcome_user()
 
+welcome_user()
