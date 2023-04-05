afe9255b42b37ef9af390cfd81e2b73b67c58031
saintodes <odes36@gmail.com>
2023-04-05 21:02:22 +0700

    adding brain-even game engine

---
+++ brain_games/games/game_even.py
@@ -0,0 +1,16 @@
+#
+#
+#
+from random import randint
+
+QUESTION_TEXT = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+def get_question_and_answer():
+    difficullty_min = 1
+    difficullty_max = 100
+    question = randint(difficullty_min, difficullty_max)
+    if question % 2 == 0:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return question, answer
