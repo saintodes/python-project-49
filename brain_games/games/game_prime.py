44fd0f7bb04e26fbb5dda6b22b7abc8e33e3e70b
saintodes <odes36@gmail.com>
2023-04-05 21:41:30 +0700

    implemented new architecture

---
+++ brain_games/games/game_prime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from random import randint
+
+
+QUESTION_TEXT = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def get_question_and_answer():
+    divider_count = 0
+    difficullty_min = 1
+    difficullty_max = 10
+    value = randint(difficullty_min, difficullty_max)
+    question = f'{value}'
+    for divider in range(1, value + 1):
+        if value % divider == 0:
+            divider_count += 1
+    if divider_count == 2:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return question, answer
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,56 +1,10 @@
 #!/usr/bin/env python3
-from random import randint
-from brain_games.game_engine import welcome_user
 
-games_played_counter = 0
-
-
-def get_correct_answer():
-    global correct_answer, digit
-    digit = randint(1, 10)
-    count = 0
-    for i in range(1, digit + 1):
-        if digit % i == 0:
-            count += 1
-    if count == 2:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-
-
-def print_rules_and_question():
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    print(f'Question: {digit}')
-
-
-def get_user_answer():
-    global user_answer
-    user_answer = input()
-
-
-def compare_results():
-    global games_played_counter
-    if correct_answer == user_answer:
-        print('Correct!')
-        games_played_counter += 1
-        if games_played_counter == 3:
-            print(f'Congratulations, {name}!')
-    else:
-        print(f"'{user_answer}' is wrong answer ;(. "
-              f"Correct answer was '{correct_answer}'")
-        print(f"Let's try again, {name}")
-        games_played_counter = 3
-
+from brain_games.common_games_engine import run_game
+from brain_games.games.game_prime import QUESTION_TEXT, get_question_and_answer
 
 def main():
-    global games_played_counter, name
-    name = welcome_user()
-    while games_played_counter != 3:
-        get_correct_answer()
-        print_rules_and_question()
-        get_user_answer()
-        compare_results()
+    run_game(QUESTION_TEXT, get_question_and_answer)
 
-
-if __name__ == '__main__':
+if __name__ == ' __main__':
     main()
