0fc0f36d454000ed8650d29d338a72a447347db8
saintodes <odes36@gmail.com>
2023-04-06 01:43:58 +0700

    tuple to string format added

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,26 +1,28 @@
-#
-#
-#
 from random import randint
 
 QUESTION_TEXT = 'What number is missing in the progression?'
 
 
 def get_question_and_answer():
-    numberic_progression = []
+    numeric_progression = []
     progression_first_digit_random_from = 1
     progression_first_digit_random_to = 25
     progression_step_random_from = 5
     progression_step_random_to = 10
 
-    first_number = randint(progression_first_digit_random_from, progression_first_digit_random_to)
+    first_number = randint(progression_first_digit_random_from,
+                           progression_first_digit_random_to)
+    numeric_progression.append(first_number)
+    progression_step = randint(progression_step_random_from,
+                               progression_step_random_to)
 
-    numberic_progression.append(first_number)
-    progression_step = randint(progression_step_random_from, progression_step_random_to)
-    while len(numberic_progression) != 10:
-        numberic_progression.append(numberic_progression[-1]
-                                    + progression_step)
-    random_index = randint(0, len(numberic_progression) - 1)
-    answer = numberic_progression[random_index]
-    numberic_progression[random_index] = '..'
-    return numberic_progression, str(answer)
+    for _ in range(9):  # We add 9 more numbers since the first number is already added
+        numeric_progression.append(numeric_progression[-1] + progression_step)
+
+    random_index = randint(0, len(numeric_progression) - 1)
+    answer = numeric_progression[random_index]
+    numeric_progression[random_index] = '..'
+
+    formatted_progression = ' '.join(map(str, numeric_progression))
+
+    return formatted_progression, str(answer)
