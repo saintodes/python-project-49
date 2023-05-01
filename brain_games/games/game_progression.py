84421c73df02aaf7bd8f4ffe2e666a70f2020877
saintodes <odes36@gmail.com>
2023-05-01 22:32:11 +0700

    separate progression gen

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -8,6 +8,20 @@ STEP_MIN = 5
 STEP_MAX = 10
 PROGRESSION_LENGTH = 10
 
+def generate_progression(first_number, step, length):
+    """
+    Function to generate a numeric progression.
+
+    Parameters:
+        first_number (int): The first number in the progression.
+        step (int): The common difference between all numbers in the progression.
+        length (int): The total length of the progression.
+    
+    Returns:
+        list of int: The generated progression.
+    """
+    return [first_number + i*step for i in range(length)]
+
 def get_question_and_answer():
     """
     Function to generate a question and its answer for finding the missing number in a progression.
@@ -17,13 +31,10 @@ def get_question_and_answer():
         question (str): The progression string with one number replaced by '..'.
         answer (str): The missing number in the progression as a string.
     """
-    progression = []
     first_number = randint(FIRST_DIGIT_MIN, FIRST_DIGIT_MAX)
-    progression.append(first_number)
     step = randint(STEP_MIN, STEP_MAX)
 
-    for _ in range(PROGRESSION_LENGTH - 1):  # Subtract 1 because the first number is already added
-        progression.append(progression[-1] + step)
+    progression = generate_progression(first_number, step, PROGRESSION_LENGTH)
 
     missing_index = randint(0, len(progression) - 1)
     answer = progression[missing_index]
