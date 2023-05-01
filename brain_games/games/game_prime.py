beae6a4ed059f028734a2dddc3980ad11ec13eee
saintodes <odes36@gmail.com>
2023-05-01 22:27:26 +0700

    missclick

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -8,26 +8,7 @@ DIFFICULTY_MAX = 10
 def is_prime(n):
     """
     Function to check if a number is prime.
-    from random import randint
-
-QUESTION_TEXT = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-
-
-def get_question_and_answer():
-    divider_count = 0
-    difficullty_min = 1
-    difficullty_max = 10
-    value = randint(difficullty_min, difficullty_max)
-    question = f'{value}'
-    for divider in range(1, value + 1):
-        if value % divider == 0:
-            divider_count += 1
-    if divider_count == 2:
-        answer = 'yes'
-    else:
-        answer = 'no'
-    return question, answer
-
+    
     Parameters:
         n (int): The number to check.
     
