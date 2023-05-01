8093b28ee845a9287b09c49ee45ec1bff60dfdd6
saintodes <odes36@gmail.com>
2023-05-01 21:50:16 +0700

    refactor, add comments

--- brain_games/common_games_engine.py
+++ brain_games/common_games_engine.py
@@ -1,29 +1,63 @@
+# Importing required modules
 from prompt import string
+from itertools import islice, repeat
 
-
+# Function to run the game
 def run_game(task, question_and_answer):
+    # Printing welcome message
     print('Welcome to the Brain Games!')
+
+    # Asking the user for their name
     username = string('May I have your name? ')
+
+    # Printing personalized welcome message
     print(f'Hello, {username}!')
-    print(f'{task}')  # объявляем задание
-    games_played_counter = 0
-    while games_played_counter != 3:
+
+    # Printing task description
+    print(f'{task}')  
+
+    # Function to play a round of game
+    def play_round():
+        # Fetching question and correct answer
         question, correct_answer = question_and_answer()
+
+        # Displaying question
         print(f'Question: {question}')
+
+        # Accepting user's answer
         user_answer = string('Your answer: ')
-        if user_answer == correct_answer:
-            print('Correct!')
-            games_played_counter += 1
-            if games_played_counter == 3:
-                print(f'Congratulations, {username}!')
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'")
-            print(f"Let's try again, {username}!")
-            break
 
+        # Asserting if user's answer is equal to correct answer
+        assert user_answer.lower() == correct_answer.lower(), \
+            f"'{user_answer}' is wrong answer ;(. " \
+            f"Correct answer was '{correct_answer}'" \
+            f"\nLet's try again, {username}!"
 
+        # Printing correct answer message
+        print('Correct!')
+
+        # Returning True for correct answer
+        return True
+
+    # Running game for 3 rounds
+    try:
+        rounds_played = sum(play_round() for _ in range(3))
+
+        # Printing congratulations message on successfully playing 3 rounds
+        print(f'Congratulations, {username}!')
+
+    # Catching AssertionError if any, which is raised when user's answer doesn't match correct answer
+    except AssertionError as error:
+        # Printing error message
+        print(error)
+
+# Function to welcome the user
 def welcome_user():
+    # Asking the user for their name
     name = string('May I have your name? ')
+
+    # Printing personalized welcome message
     print(f'Hello, {name}!')
+
+    # Printing welcome message for the game
     print('Welcome to the Brain Games!')
